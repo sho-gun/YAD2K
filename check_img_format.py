d9c8cd4251671b656e7c09bce4a5ad2973ac8d59
Masaki Hosono <office@utagoe.com>
2020-05-12 16:14:33 +0900

    accept directory as input

--- check_img_format.py
+++ check_img_format.py
@@ -13,13 +13,26 @@ parser.add_argument(
 def _main(args):
     image_path = os.path.expanduser(args.image_path)
 
-    img = np.array(Image.open(image_path))
-    print(img.shape)
-    if img.shape[2] == 4:
-        img = img[:, :, :3]
-        img = Image.fromarray(img)
-        # img.show()
-        img.save('output_' + image_path)
+    if os.path.isdir(image_path):
+        for image in os.listdir(image_path):
+            file_path = os.path.join(image_path, image)
+
+            img = np.array(Image.open(file_path))
+            print(img.shape)
+            if img.shape[2] == 4:
+                img = img[:, :, :3]
+                img = Image.fromarray(img)
+                # img.show()
+                img.save(os.path.join('output_' + image))
+
+    else:
+        img = np.array(Image.open(image_path))
+        print(img.shape)
+        if img.shape[2] == 4:
+            img = img[:, :, :3]
+            img = Image.fromarray(img)
+            # img.show()
+            img.save('output_' + image_path)
 
 if __name__ == '__main__':
     _main(parser.parse_args())
